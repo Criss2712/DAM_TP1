e8b70c46e5196706ad0bb131a778bc9d01249cca
Criss2712 <renata2002.gois@gmail.com>
2026-03-04 08:08:51 +0000

    Commit 2: Ex.1 Concluído/Comentado

--- src/main/kotlin/dam/exer_1/exer_1.kt
+++ src/main/kotlin/dam/exer_1/exer_1.kt
@@ -1,7 +1,7 @@
 package org.example.dam.exer_1
 
 fun main() {
-    //Alínea a): Using IntArray constructor
+    //Alínea (a): Using IntArray constructor
     // Array com 50 elementos inicializados a 0
     val a = IntArray(50)
     // Percorrer os valores de 1 a 50 para calcular o quadrado de cada um
@@ -11,12 +11,12 @@ fun main() {
     }
     print("(a) IntArray Constructor: " + a.toList())
 
-    //Alínea b): Using a range and map()
+    //Alínea (b): Using a range and map()
     // Utilização da transformação map() com função lambda que calcula o quadrado de cada valor
     val b = (1..50).map{it * it}
     print("\n(b) Range and Map: " + b.toList())
 
-    //Alínea c): Using Array with constructor
+    //Alínea (c): Using Array with constructor
     // Utilização de um construtor de Arrays que preenche o array com o quadrado de cada valor de 1 a 50
     val c = Array(50) {i -> ((i+1)*(i+1))}
     print("\n(c) Array Constructor: " + c.toList())
