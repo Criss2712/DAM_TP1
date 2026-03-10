6729586a1ee1c21b7054c66e425dc12caf73e6f4
Criss2712 <renata2002.gois@gmail.com>
2026-03-10 08:28:31 +0000

    Ex.3: Generate Sequence

--- src/main/kotlin/dam/exer_3/exer_3.kt
+++ src/main/kotlin/dam/exer_3/exer_3.kt
@@ -1,5 +1,14 @@
 package org.example.dam.exer_3
 
 fun main() {
-    println("Hello World!")
+    println("+++++++ Sequência de Saltos +++++++")
+    // Altura inicial: 100
+    // Função: it*0.6 -> pega no valor anterior e calcula o próximo
+    val bounceSequence = generateSequence(100.0) {it * 0.6}
+        .take(15)          // Os primeiros 15 ressaltos
+        .filter { it >= 1.0 } // Filtrar para obter apenas ressaltos superiores a 1 metro
+        .toList()             // Conversão para lista
+
+    // Apresentação da sequência com duas casas decimais
+    bounceSequence.forEach {println(String.format("%.2f", it))}
 }
